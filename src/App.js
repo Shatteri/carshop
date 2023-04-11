f03fa596c33ba0370c368425898e65559d8e0723
Shatteri <elias.isopahkala@gmail.com>
2023-04-11 22:14:30 +0300

    testis

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import Carlist from './components/Carlist';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Carlist from './components/Carlist';
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            CarShop
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Carlist />
+      <Carlist/>
     </div>
   );
 }
